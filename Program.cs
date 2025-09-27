33a4ee977352ab9e4475ac0147c09ceac0d18496
Anderson Sanchez <anderson.asm201@gmail.com>
2025-09-27 18:42:12 -0500

    Menú principal con opciones de iniciar sesión, registrar usuario y salir.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,41 @@ namespace Cajero_automatico
     {
         static void Main(string[] args)
         {
+            bool salir = false;
+
+            while (!salir) 
+            { 
+                Console.Clear();
+                Console.WriteLine(" === CAJERO AUTOMATICO === ");
+                Console.WriteLine("1. Iniciar sesión");
+                Console.WriteLine("2. Registrar usuario");
+                Console.WriteLine("3. Salir");
+                Console.Write("3. Seleccione una opcion: ");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion) 
+                {
+                    case "1":
+                        Console.WriteLine("\n[Opcion Iniciar sesión todavia no implementada]");
+                        Console.ReadKey();
+                        break;
+                    
+                    case "2":
+                        Console.WriteLine("\n[Opcion Registrar usuario todavia no implementada]");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("\n[Saliendo del sistema]");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("\n Opcion no válida.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
         }
     }
 }
